b5c05da2fa23324e12f36373aa230b740c90392e
Shreya Jayaraj <shreya291104@gmail.com>
2024-07-31 09:49:12 +0530

    added env file for backend

--- csv-server/server.js
+++ csv-server/server.js
@@ -1,63 +1,8 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const fs = require('fs');
-// const path = require('path');
-
-// const app = express();
-// app.use(bodyParser.json());
-
-// const cors = require('cors');
-// app.use(cors());
-
-// // Paths to CSV files
-// const formTutorCsvPath = path.join(__dirname, 'tutor_data.csv');
-// const formTuitionCsvPath = path.join(__dirname, 'tuition_data.csv');
-
-// // Create CSV files if they don't exist
-// if (!fs.existsSync(formTutorCsvPath)) {
-//   fs.writeFileSync(formTutorCsvPath, 'name,email,contact,qualifications,subjects,location,date,time\n');
-// }
-// if (!fs.existsSync(formTuitionCsvPath)) {
-//   fs.writeFileSync(formTuitionCsvPath, 'name,contact,class,location,date,time\n');
-// }
-
-// // Route for FormTutor data
-// app.post('/save-tutor-data', (req, res) => {
-//   const { name, email, contact, qualifications, subjects, location, date, time } = req.body;
-//   const line = `${name},${email},${contact},${qualifications},${subjects},${location},${date},${time}\n`;
-
-//   fs.appendFile(formTutorCsvPath, line, err => {
-//     if (err) {
-//       return res.status(500).send('Error saving data');
-//     }
-//     res.send('Data saved successfully');
-//   });
-// });
-
-// // Route for FormTuition data
-// app.post('/save-tuition-data', (req, res) => {
-//   const { name, contact, class: studentClass, location, date, time } = req.body;
-//   const line = `${name},${contact},${studentClass},${location},${date},${time}\n`;
-
-//   fs.appendFile(formTuitionCsvPath, line, err => {
-//     if (err) {
-//       return res.status(500).send('Error saving data');
-//     }
-//     res.send('Data saved successfully');
-//   });
-// });
-
-// const PORT = 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
@@ -118,7 +63,7 @@ app.post('/save-merit-data', (req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
